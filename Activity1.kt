bdc4c1b305e1a919b38b992ccd3867b7cac5dc1f
tanaka-tutz <94239507+TanakaMasitera@users.noreply.github.com>
2026-09-17 20:52:39 -0700

    Complete Activity 1 Kotlin exercises.

---
+++ Activity1.kt
@@ -0,0 +1,24 @@
+fun main() {
+    // My first Kotlin activity
+    println("Hello, Tanaka!")
+
+    // Practising printing multiple lines
+    println("I'm")
+    println("learning")
+    println("Kotlin!")
+
+    // Practising the order of output
+    println("Monday")
+    println("Tuesday")
+    println("Wednesday")
+    println("Thursday")
+    println("Friday")
+
+    // More practice with println()
+    println("Tomorrow is rainy")
+    println("There is a chance of snow")
+    println("Cloudy")
+    println("Partly Cloudy")
+    println("Windy")
+    println("How's the weather today?")
+}
